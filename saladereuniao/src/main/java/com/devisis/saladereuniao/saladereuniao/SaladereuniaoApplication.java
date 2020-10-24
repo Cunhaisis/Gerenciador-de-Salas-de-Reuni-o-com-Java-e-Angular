090411d537e12c2e1012fd8bc6f1b027817f03b6
Isis priscila <isis.cunha13!gmail.com>
2020-10-23 21:09:40 -0300

    Backend finalizado

--- saladereuniao/src/main/java/com/devisis/saladereuniao/saladereuniao/SaladereuniaoApplication.java
+++ saladereuniao/src/main/java/com/devisis/saladereuniao/saladereuniao/SaladereuniaoApplication.java
@@ -2,8 +2,10 @@ package com.devisis.saladereuniao.saladereuniao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"/saladereuniao/src/main/java/com/devisis/saladereuniao/saladereuniao/SaladereuniaoApplication.java"})
 public class SaladereuniaoApplication {
 
 	public static void main(String[] args) {
